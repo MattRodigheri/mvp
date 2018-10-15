9f20b6f8e1699c9e40327fa1706695f0a7b6db66
MattRodigheri <mrodigheri@gmail.com>
2018-10-14 17:00:35 -0700

    math.round data

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -5,25 +5,25 @@ function Display(props) {
 
   return (
     <div className='display'>
-      <div>Number of Asteroids: {props.asteroids.elements}</div>
+      <div>Count: {props.asteroids.elements}</div>
       <div>
         {
           props.asteroids.near_earth_objects.map((element) => {
-          var count = 0;
-          count++;
-          var hazardous = element.is_potentially_hazardous_asteroid ? <div>Potentially Hazardous</div> : null;
-          return (
-            <div key={element.name} className='element'>
-              <div>Name: {element.name}</div>
-              <div>Diameter: {element.estimated_diameter.feet.estimated_diameter_max}</div>
-              <div>Velocity: {element.close_approach_data[0].relative_velocity.miles_per_hour}</div>
-              <div>Distance from Earth: {element.close_approach_data[0].miss_distance.miles}</div>
-              <div>{hazardous}</div>
-              <img className='asteroidImg'/>
-            </div>
-          )
-        })
-      }
+            var hazardous = element.is_potentially_hazardous_asteroid ? <div>Potentially Hazardous</div> : null;
+            return (
+              <div key={element.name} className='element'>
+                <image className='elementImage'/>
+                <div className='elementText'>
+                  <div>Name: {element.name}</div>
+                  <div>Diameter: {Math.round(element.estimated_diameter.feet.estimated_diameter_max)} feet</div>
+                  <div>Velocity: {Math.round(element.close_approach_data[0].relative_velocity.miles_per_hour)} mph</div>
+                  <div>Distance from Earth: {Math.round(element.close_approach_data[0].miss_distance.miles)} miles</div>
+                  <div className='hazardous'>{hazardous}</div>
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
